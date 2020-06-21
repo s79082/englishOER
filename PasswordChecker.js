1f11c567b840fb69a9dbabf5487af44feeff99e5
Moritz <s79082@htw-dresden.de>
2020-06-21 19:42:20 +0200

    added jsmind for mindmaps

---
+++ PasswordChecker.js
@@ -0,0 +1,25 @@
+class PasswordChecker {
+    constructor(text)
+    {
+        this.text = text;
+    }
+    getChecks()
+    {
+        return [{"label": "At least one Lowercase letter",
+        "checksOut":(new RegExp("[a-z]")).test(this.text)
+    },
+    {"label": "At least one Uppercase letter",
+        "checksOut":(new RegExp("[A-Z]")).test(this.text)
+    },
+    {"label": "At least one Special letter",
+        "checksOut": (new RegExp("[$@$!%*#?&]")).test(this.text)
+    },
+    {"label": "At least one Number letter",
+        "checksOut": (new RegExp("[0-9]")).test(this.text)
+    },
+    {"label": "At least 8 letters",
+        "checksOut": (this.text.length >= 8)
+    }];
+    }
+
+}
